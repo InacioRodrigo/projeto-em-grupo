c5d1d1ef1db5f15d8b24377c8bb5cdd292864a55
inaciorodrigo <rodrigo.silva@sptech.school>
2022-11-04 08:50:06 -0300

    tela de login for business

---
+++ script/validarLogin.js
@@ -0,0 +1,30 @@
+function validarLogin() {
+    var emailLogin = login.value
+    var senhaLogin = senha.value
+    const conectando = document.querySelector("#conectar");
+
+    if (emailLogin=='' || senhaLogin=='') {
+        alert('Preencha todos os campos')
+    } else if (emailLogin=='user123@fleming.com' && senhaLogin== 123) {
+        conectando.href = "login_for_business.html"
+    } else {
+        alert('Email ou Senha invalidos. Tente novamente')
+    }
+}
+
+function validarLoginBusiness() {
+    var emailLogin = login.value
+    var senhaLogin = senha.value
+    const conectando = document.querySelector("#conectar");
+
+    if (emailLogin=='' || senhaLogin=='') {
+        alert('Preencha todos os campos')
+    } else if (emailLogin=='user123@fleming.com' && senhaLogin== 123) {
+        conectando.href = "dashBoard.html"
+    } else {
+        alert('Email ou Senha invalidos. Tente novamente')
+    }
+}
+
+
+

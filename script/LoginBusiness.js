47a156659b75a5f62b557b918a3478e8b7da87f2
rodrigoinacio <rodrigo.silva@sptech.school>
2022-11-11 15:23:41 -0300

    Campo para as funções do login for business

---
+++ script/LoginBusiness.js
@@ -0,0 +1,18 @@
+function validarLoginBusiness() {
+    var emailLogin = login.value
+    var senhaLogin = senha.value
+    const conectando = document.querySelector("#conectar");
+
+    if (emailLogin=='' || senhaLogin=='') {
+        alert('Preencha todos os campos')
+    } else if (emailLogin=='user123@fleming.com' && senhaLogin== 123) {
+        conectando.href = "dashBoard.html"
+    } else {
+        alert('Email ou Senha invalidos. Tente novamente')
+    }
+}
+
+function loginBusiness() {
+    const conectando = document.querySelector("#conectado");
+    conectando.href = "login_for_business.html";
+}
--- script/validarLogin.js
+++ script/validarLogin.js
@@ -11,22 +11,3 @@ function validarLogin() {
         alert('Email ou Senha invalidos. Tente novamente')
     }
 }
-
-function validarLoginBusiness() {
-    var emailLogin = login.value
-    var senhaLogin = senha.value
-    const conectando = document.querySelector("#conectar");
-
-    if (emailLogin=='' || senhaLogin=='') {
-        alert('Preencha todos os campos')
-    } else if (emailLogin=='user123@fleming.com' && senhaLogin== 123) {
-        conectando.href = "dashBoard.html"
-    } else {
-        alert('Email ou Senha invalidos. Tente novamente')
-    }
-}
-
-function loginBusiness() {
-    const conectando = document.querySelector("#conectado");
-    conectando.href = "login_for_business.html";
-}
